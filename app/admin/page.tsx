41dd446a0a70405fc1717f21466e162b3e549b0b
Giulio Zulian <37471491+giuliozulian@users.noreply.github.com>
2025-11-17 10:29:40 +0100

    feat: add navigation links for People, Projects, and Scheduling in Home component

---
+++ app/admin/page.tsx
@@ -0,0 +1,36 @@
+import { Calendar, FolderKanban, PersonStanding } from 'lucide-react';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-start justify-start">
+      <main className="container mx-auto">
+        <div className="flex flex-col items-start gap-8 text-base font-medium sm:flex-row">
+          <Link
+            className="flex min-h-40 w-1/3 items-center gap-4 rounded-lg border border-[#555] bg-[#333] p-12 text-2xl text-white hover:bg-[#444] focus:ring-2 focus:ring-[#666] focus:ring-offset-2 focus:outline-none"
+            href="/people"
+          >
+            <PersonStanding className="h-16 w-16 stroke-1" />
+            <h2>People</h2>
+          </Link>
+
+          <Link
+            className="flex min-h-40 w-1/3 items-center gap-4 rounded-lg border border-[#555] bg-[#333] p-12 text-2xl text-white hover:bg-[#444] focus:ring-2 focus:ring-[#666] focus:ring-offset-2 focus:outline-none"
+            href="/projects"
+          >
+            <FolderKanban className="h-16 w-16 stroke-1" />
+            <h2>Projects</h2>
+          </Link>
+
+          <Link
+            className="flex min-h-40 w-1/3 items-center gap-4 rounded-lg border border-[#555] bg-[#333] p-12 text-2xl text-white hover:bg-[#444] focus:ring-2 focus:ring-[#666] focus:ring-offset-2 focus:outline-none"
+            href="/scheduling"
+          >
+            <Calendar className="h-16 w-16 stroke-1" />
+            <h2>Scheduling</h2>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,8 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between px-16 py-32 sm:items-start">
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <Link
-            className="flex h-12 w-full items-center justify-center rounded-full bg-blue-600 px-5 text-white transition-colors hover:bg-blue-700 md:w-[158px]"
-            href="/people"
-          >
-            Vai alla tabella People
-          </Link>
-          <Link
-            className="flex h-12 w-full items-center justify-center rounded-full bg-green-600 px-5 text-white transition-colors hover:bg-green-700 md:w-[158px]"
-            href="/projects"
-          >
-            Vai alla tabella Projects
-          </Link>
-        </div>
+    <div className="flex min-h-screen items-start justify-start">
+      <main className="container mx-auto">
+        <h2 className="text-xl">Welcome</h2>
       </main>
     </div>
   );
